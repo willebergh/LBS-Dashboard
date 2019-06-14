c9890037911f748b3089a245683abe6ba8e2c981
William Bergh <willebergh@outlook.com>
2019-06-14 04:01:16 +0200

    added forecast component

---
+++ src/client/src/components/weather/Forecast.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Forecast extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Forecast;
