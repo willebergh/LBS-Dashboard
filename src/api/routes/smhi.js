48d6e996639de4bc2467277d2e265344de58551c
William Bergh <willebergh@outlook.com>
2019-06-17 11:25:10 +0200

    changed cords in the api request

--- src/api/routes/smhi.js
+++ src/api/routes/smhi.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const axios = require("axios");
 
 router.get("/forecast", (req, res) => {
-    axios.get("https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18/lat/59/data.json")
+    axios.get("https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.06263/lat/59.32946/data.json")
         .then(response => res.status(200).json(response.data))
         .catch(err => res.status(400).json(err))
 });
