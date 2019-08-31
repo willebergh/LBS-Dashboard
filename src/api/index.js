83a4fa3670542007cba3b54ecd5eeaf32741ce19
willebergh <willebergh@outlook.com>
2019-08-31 11:20:28 +0200

    commiting backend changes too

--- src/api/index.js
+++ src/api/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use("/sl", require("./routes/sl"));
 router.use("/smhi", require("./routes/smhi"));
+router.use("/food", require("./routes/food"));
 
 module.exports = router;
--- src/api/routes/smhi.js
+++ src/api/routes/smhi.js
@@ -1,11 +1,69 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+const moment = require("moment")
 
 router.get("/forecast", (req, res) => {
     axios.get("https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.06263/lat/59.32946/data.json")
         .then(response => res.status(200).json(response.data))
         .catch(err => res.status(400).json(err))
+});
+
+router.get("/current", (req, res) => {
+    axios.get("https://opendata-download-metanalys.smhi.se/api/category/mesan1g/version/2/geotype/point/lon/18.06263/lat/59.32946/data.json")
+        .then(response => res.status(200).json(response.data))
+        .catch(err => res.status(400).json(err))
+});
+
+router.get("/test", async (req, res) => {
+
+    const timeSeries = [];
+
+    await axios.get("https://opendata-download-metanalys.smhi.se/api/category/mesan1g/version/2/geotype/point/lon/18.06263/lat/59.32946/data.json")
+        .then(response => {
+            response.data.timeSeries.reverse().forEach(time => {
+                timeSeries.push(time)
+            })
+        });
+
+    await axios.get("https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.06263/lat/59.32946/data.json")
+        .then(response => {
+
+            response.data.timeSeries.forEach(time => {
+                timeSeries.push(time)
+            })
+
+            response.data.timeSeries = timeSeries
+
+            res.status(200).json(response.data)
+
+        });
+
+});
+
+router.get("/test2", async (req, res) => {
+
+    const timeSeries = [];
+
+    await axios.get("https://opendata-download-metanalys.smhi.se/api/category/mesan1g/version/2/geotype/point/lon/18.06263/lat/59.32946/data.json")
+        .then(response => {
+            response.data.timeSeries.reverse().forEach(time => {
+                timeSeries.push(time.validTime)
+            })
+        });
+
+    await axios.get("https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.06263/lat/59.32946/data.json")
+        .then(response => {
+
+            response.data.timeSeries.forEach(time => {
+                timeSeries.push(time.validTime)
+            })
+
+
+            res.status(200).json(timeSeries)
+
+        });
+
 });
 
 module.exports = router;
