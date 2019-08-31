e1e8d319beb888ba2f842820c226890c32c2551e
willebergh <willebergh@outlook.com>
2019-08-31 12:12:25 +0200

    made all express routes load dynamically based on amount of files in routes dir

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
+const path = require("path");
 
-router.use("/sl", require("./routes/sl"));
-router.use("/smhi", require("./routes/smhi"));
-router.use("/food", require("./routes/food"));
+const dir = path.join(__dirname, "routes");
+const routes = fs.readdirSync(dir);
+
+for (var i in routes) {
+    const fileName = path.basename(routes[i], ".js");
+    const filePath = path.join(dir, routes[i]);
+    router.use(`/${fileName}`, require(filePath));
+}
 
 module.exports = router;
