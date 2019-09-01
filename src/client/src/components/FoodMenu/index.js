1390a203bc673408f978f8d55310e3132bc05e85
BuildTools <willebergh@outlook.com>
2019-09-01 21:28:17 +0200

    created todays foodmenu component

---
+++ src/client/src/components/FoodMenu/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import moment from "moment";
+import axios from "axios";
+import "./style.css";
+
+
+class FoodMenu extends Component {
+    constructor() {
+        super();
+        this.state = {
+            menuItems: []
+        }
+    }
+
+    componentDidMount() {
+        this.clock();
+    }
+
+    clock() {
+        this.updateState();
+        setInterval(() => {
+            const time = moment().format("HH:mm:ss");
+            if (time === "00:00:00") this.updateState();
+        }, 1000)
+    }
+
+    updateState() {
+        axios.get("/api/FoodMenu/today")
+            .then(res => {
+                this.setState({ menuItems: res.data.today.menu })
+            })
+    }
+
+    render() {
+        return (
+            <div id="foodMenu">
+                <div className="header">
+                    <span>Idag @ Jöns Jacob</span>
+                </div>
+                <div className="body">
+                    {this.state.menuItems.map(item => {
+                        return (
+                            <span className="item">
+                                {item}
+                            </span>
+                        )
+                    })}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default FoodMenu;
