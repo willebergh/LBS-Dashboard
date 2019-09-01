b4358296a8f77f3264b3efcb5607256e066266be
BuildTools <willebergh@outlook.com>
2019-09-01 18:11:24 +0200

    added Departures component to App

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -4,12 +4,14 @@ import "./App.css";
 // Components
 import CurrentTimeAndDate from "./components/CurrentTimeAndDate";
 import Weather from "./components/Weather";
+import Departures from "./components/Departures";
 
 function App() {
   return (
     <div id="grid">
       <CurrentTimeAndDate />
       <Weather />
+      <Departures station="3404" transportType="bus" />
     </div>
   );
 }
