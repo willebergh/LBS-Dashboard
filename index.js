5064718211d79012d14dde65b96e769a301d0eb1
willebergh <willebergh@outlook.com>
2019-09-02 13:24:47 +0200

    added logger and database.init() function

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const logger = require("./src/logger");
+
+const database = require("./src/database");
+database.init();
 
 app.use(express.json());
 app.use(cors({
@@ -11,4 +15,6 @@ const routes = require("./src/routes");
 app.use(routes);
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(PORT, () => {
+    logger.log("Express", `Server started on port ${PORT}.`)
+})
