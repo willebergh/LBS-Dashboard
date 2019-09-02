2f0ee9d5068f80e37f20c43e480ad81de7fdea51
willebergh <willebergh@outlook.com>
2019-09-02 12:48:34 +0200

    added mongoose mondel for Forecasts

---
+++ src/models/Forecasts.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const ForecastSchema = new mongoose.Schema({
+    latitude: {
+        type: Number
+    },
+    longitude: {
+        type: Number
+    },
+    timeZone: {
+        type: String
+    },
+    currently: {
+        type: Object
+    },
+    hourly: {
+        type: Object
+    },
+    daily: {
+        type: Object
+    },
+    alerts: {
+        type: Array
+    },
+    flags: {
+        type: Object
+    },
+    offset: {
+        type: Number
+    }
+});
+
+module.exports = CurrentlySchema;
