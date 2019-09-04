76e80c14fc8bbcbe106c8fe4df8200c15f6448e5
BuildTools <willebergh@outlook.com>
2019-09-04 21:20:30 +0200

    renamed file and added sl station update function

--- src/database/updater/updateSLRealTime.js
+++
@@ -1,8 +0,0 @@
-const axios = require("axios");
-const logger = require("../../logger");
-
-function updateSlRealTime() {
-    logger.log("Updating SL RealTime...", "Updater")
-}
-
-module.exports = updateSlRealTime;
---
+++ src/database/updater/updateStation.js
@@ -0,0 +1,38 @@
+const axios = require("axios");
+const logger = require("../../logger");
+const Station = require("../../models/Station");
+require("dotenv").config();
+
+module.exports = async function (siteId) {
+    if (!siteId) return;
+
+    logger.log(`Updating station ${siteId}...`, "Updater");
+
+    const data = await getData(siteId);
+    const station = await Station.findOne({ siteId });
+
+    if (!station) {
+        const newStation = new Station(data)
+        newStation.save()
+            .then(() => logger.log(`Added station ${siteId} to the database`, "Updater"))
+            .catch(err => logger.error(err, "Updater"))
+    } else {
+        await Station.deleteOne({ _id: station._id });
+        const newStation = new Station(data);
+        newStation.save()
+            .then(() => logger.log(`Updated station ${siteId}`, "Updater"))
+            .catch(err => logger.error(err, "Updater"))
+    }
+}
+
+async function getData(siteId) {
+    let data;
+
+    const token = process.env.SL_API_REALTIME_TOKEN;
+    const url = `https://api.sl.se/api2/realtimedeparturesV4.json?key=${token}&siteid=${siteId}`;
+    await axios.get(url)
+        .then(res => data = { siteId, ...res.data.ResponseData })
+        .catch(err => logger.error(err, "Updater"))
+
+    return data;
+}
