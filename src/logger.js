297ade286909b16360f7578cce4c24717bcb9b9d
willebergh <willebergh@outlook.com>
2019-09-04 15:04:57 +0200

    added colors to logger

--- src/logger.js
+++ src/logger.js
@@ -1,10 +1,14 @@
+const colors = require("colors");
 
 // Console log message
-module.exports.log = function log(source, message) {
-    console.log(`[${source}] ${message}`)
+module.exports.log = function log(...args) {
+    const message = args[0];
+    const tags = args.slice(1).map(t => `[${t}]`.cyan);
+    console.log(...tags, message);
 }
 
 // Console log error
-module.exports.err = function err(source, error) {
-    console.error(`[${source}]` + error)
+module.exports.error = function (...args) {
+    const tags = args.length > 1 ? args.slice(0, -1).map(t => `[${t}]`.red) : []
+    console.error('[ErrorLog]'.red, ...tags, "from", args[args.length - 1].underline.cyan)
 }
