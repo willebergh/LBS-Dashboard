e3baae30a461d0c761c7f38b22b706e4e52e9235
willebergh <willebergh@outlook.com>
2019-09-04 15:08:14 +0200

    restructured index.js

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const logger = require("./src/logger");
 
+const logger = require("./src/logger");
+const routes = require("./src/routes");
 const database = require("./src/database");
-database.init();
 
 app.use(express.json());
-app.use(cors({
-    "Access-Control-Allow-Origin": "*"
-}))
-
-const routes = require("./src/routes");
 app.use(routes);
+
+database.init();
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-    logger.log("Express", `Server started on port ${PORT}.`)
+    logger.log(`Server started on port ${PORT}.`, "Express")
 })
