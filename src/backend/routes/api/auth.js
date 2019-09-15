d43e35a089320fb58211c323f05949413710aa63
BuildTools <willebergh@outlook.com>
2019-09-15 15:54:16 +0200

    added route for backend authentiction

---
+++ src/backend/routes/api/auth.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const admin = require("../../config/firebase-admin");
+
+router.post("/login", (req, res) => {
+    const idToken = req.body.idToken;
+    admin.auth().verifyIdToken(idToken)
+        .then(() => {
+            req.session.idToken = idToken;
+            req.session.save(err => {
+                if (err) throw err;
+                res.status(200).json({ msg: "success" })
+            })
+        })
+        .catch(err => {
+            res.send(err)
+        });
+});
+
+router.get("/logout", (req, res) => {
+    req.session.destroy(err => {
+        if (err) throw err;
+        res.status(200).json({ msg: "success" });
+    })
+});
+
+module.exports = router;
