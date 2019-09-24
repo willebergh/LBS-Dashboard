82a22a458f5e04835a4f38b136cb28bf8e5555bd
BuildTools <willebergh@outlook.com>
2019-09-24 16:45:04 +0200

    added a route for DeploymentConfigForm component

--- src/website/src/components/Admin/Deployment/index.jsx
+++ src/website/src/components/Admin/Deployment/index.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Switch, Route } from "react-router-dom";
+
+// Components
 import Dashboards from "./Dashboards/";
+import DeploymentConfigForm from "../Forms/DeploymentConfigForm";
 
 class Deployment extends Component {
     constructor() {
@@ -16,23 +19,17 @@ class Deployment extends Component {
     }
 
     render() {
-        const { deployments, socket } = this.props;
+        const { deployment, socket } = this.props;
         return (
             <Switch>
 
-                {
-                    deployments.map((deployment, i) => {
-                        return (
-                            <Route
-                                key={i}
-                                path={`/admin/${deployment.name}/dashboards`}
-                                render={props => (
-                                    <Dashboards deployment={deployment} socket={socket} {...props} />
-                                )}
-                            />
-                        );
-                    })
-                }
+                <Route path={"/admin/:deployment/dashboards"} render={props => (
+                    <Dashboards deployment={deployment} socket={socket} {...props} />
+                )} />
+
+                <Route path="/admin/:deployment/config" render={props => (
+                    <DeploymentConfigForm deployment={deployment} socket={socket} {...props} />
+                )} />
 
             </Switch>
         );
