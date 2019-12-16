a40f8de7f242b3eafc15f38a2760224ec68e595b
willebergh <willebergh@outlook.com>
2019-12-16 12:30:27 +0100

    minor change

--- src/admin-console/server.js
+++ src/admin-console/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 8080
 
-app.use(express.public("public"));
+app.use(express.static("public"));
 
 app.use("*", (req, res) => {
     res.sendFile(path.join(__dirname + "public", "index.html"));
