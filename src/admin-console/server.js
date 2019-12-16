7c2dd5455228688e1fa5f721b694ada4d433e75c
willebergh <willebergh@outlook.com>
2019-12-16 11:26:05 +0100

    changed dockerfile

--- src/admin-console/server.js
+++ src/admin-console/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 8080
 
 app.use(express.public("public"));
 
 app.use("*", (req, res) => {
     res.sendFile(path.join(__dirname + "public", "index.html"));
 });
+
+app.listen(port, () => {
+    console.log(`Serving admin-console on port ${port}`)
+})
 
