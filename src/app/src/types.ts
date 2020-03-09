1351da105f6391bf222d43339e70c39317e7c7d6
willebergh <willebergh@outlook.com>
2020-03-09 11:13:53 +0100

    added new types for IWeather.daily

--- src/app/src/types.ts
+++ src/app/src/types.ts
@@ -25,11 +25,59 @@ export interface IWeatherHourly {
     summary: string;
 }
 
+export interface IWeatherDailyData {
+    apparentTemperatureHigh: number;
+    apparentTemperatureHighTime: number;
+    apparentTemperatureLow: number;
+    apparentTemperatureLowTime: number;
+    apparentTemperatureMax: number;
+    apparentTemperatureMaxTime: number;
+    apparentTemperatureMin: number;
+    apparentTemperatureMinTime: number;
+    cloudCover: number;
+    dewPoint: number;
+    humidity: number;
+    icon: string;
+    moonPhase: number;
+    ozone: number;
+    precipIntensity: number;
+    precipIntensityMax: number;
+    precipIntensityMaxTime: number;
+    precipProbability: number;
+    precipType: string;
+    pressure: number;
+    summary: string;
+    sunriseTime: number;
+    sunsetTime: number;
+    temperatureHigh: number;
+    temperatureHighTime: number;
+    temperatureLow: number;
+    temperatureLowTime: number;
+    temperatureMax: number;
+    temperatureMaxTime: number;
+    temperatureMin: number;
+    temperatureMinTime: number;
+    time: number;
+    uvIndex: number;
+    uvIndexTime: number;
+    visibility: number;
+    windBearing: number;
+    windGust: number;
+    windGustTime: number;
+    windSpeed: number;
+}
+
+export interface IWeatherDaily {
+    data: IWeatherDailyData[];
+    icon: string;
+    summary: string;
+}
+
 export interface IWeather {
     alerts: any[];
     city: string;
     currently: IWeatherData;
-    daily: object;
+    daily: IWeatherDaily;
     flags: object;
     hourly: IWeatherHourly;
     latitude: number;
